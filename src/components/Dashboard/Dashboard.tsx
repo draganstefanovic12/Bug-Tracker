92a5d40be712b75cbb29b9e7dc45909dc7b398c6
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-09-09 19:46:20 +0200

    mobile dashboard done

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import menu from "../../assets/images/menu.svg";
 
 export const Dashboard = () => {
+  const [display, setDisplay] = useState(false);
+
+  const handleMobileDashboard = () => {
+    setDisplay(!display);
+  };
+
   return (
-    <div className="flex h-full w-44 bg-[#33475c] text-white pt-1 relative">
-      <ul className="gap-2 flex flex-col pt-2 w-44">
+    <div
+      className={
+        display
+          ? `flex h-full bg-[#33475c] text-white pt-1 w-44 animate-fadein`
+          : `w-8 animate-fadeout flex h-full bg-[#33475c] text-white pt-1`
+      }
+    >
+      <ul className="gap-2 flex flex-col pt-2 w-full">
         <img
+          onClick={handleMobileDashboard}
           src={menu}
-          className="h-6 absolute right-0 pr-1 hover:cursor-pointer sm:block md:hidden"
+          className="h-7 w-7 ml-0.5 hover:cursor-pointer sm:flex md:hidden"
           alt="menu"
         />
         <Link
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadein: "fadein 250ms ease-in-out 1 forwards",
+        fadeout: "fadeout 250ms ease-in-out 1 forwards",
+      },
+      keyframes: {
+        fadein: {
+          "0%": { width: "2rem" },
+          "100%": { width: "11rem" },
+        },
+        fadeout: {
+          "0%": { width: "11rem" },
+          "100%": { width: "2rem" },
+        },
+      },
+    },
   },
   plugins: [],
 };
