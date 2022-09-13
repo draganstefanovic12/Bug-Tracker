cbfaf141b00eb755e10cbe48f9702205e2c10ce5
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-09-13 21:52:41 +0200

    working on roles

--- src/App.tsx
+++ src/App.tsx
@@ -10,26 +10,37 @@ import { login } from "./features/user/userSlice";
 import { Login } from "./pages/Login";
 import { Ticket } from "./features/ticket/Ticket";
 import { Tickets } from "./pages/Tickets";
+import { useFetch } from "./hooks/useFetch";
 import { Projects } from "./pages/Projects";
 import { MainPage } from "./pages/MainPage";
 import { Register } from "./pages/Register";
 import { useEffect } from "react";
 import { Dashboard } from "./components/Dashboard/Dashboard";
+import { addProject } from "./features/projects/projectSlice";
 import { ProjectDetails } from "./features/projects/ProjectDetails";
 import { ProtectedRoutes } from "./components/ProtectedRoutes/ProtectedRoutes";
 import { useAppDispatch, useAppSelector } from "./hooks/useRedux";
+import { Project } from "./types/types";
+import { Roles } from "./pages/Roles";
+
+type Data = {
+  projects: Project[];
+};
 
 const App = () => {
   const user = useAppSelector((user) => user.user);
+  const data: Data = useFetch("api/projects/all")!;
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     //Checking for existing user on page load
+    //Fetching all projects on page load
+    data && dispatch(addProject(data.projects));
     const user = localStorage.getItem("BTUser");
     if (user) {
       dispatch(login(JSON.parse(user)));
     }
-  }, [dispatch]);
+  }, [data, dispatch]);
 
   return (
     <Router>
@@ -46,7 +57,7 @@ const App = () => {
           <Route path="/projects/:project" element={<ProjectDetails />} />
           <Route path="/tickets" element={<Tickets />} />
           <Route path="/tickets/:name/:title" element={<Ticket />} />
-          <Route path="/roles" />
+          <Route path="/roles" element={<Roles />} />
         </Route>
         <Route
           path="/login"
