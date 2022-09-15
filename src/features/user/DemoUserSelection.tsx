add342e8aec3c1603c25f729495bcf3e5a4bb426
draganstefanovic12 <stefanovicdragan112@gmail.com>
2022-09-16 01:13:30 +0200

    fixed css on firefox demo user

--- src/features/user/DemoUserSelection.tsx
+++ src/features/user/DemoUserSelection.tsx
@@ -26,21 +26,21 @@ export const DemoUserSelection = ({ handleDemoUser }: DemoProps) => {
   return (
     <div className="items-center flex border shadow p-5 flex-col">
       <h1 className="mb-5">Select a role you would like to preview: </h1>
-      <div className="flex gap-5">
+      <div className="flex gap-10">
         <p
-          className="before:content-user hover:cursor-pointer hover:bg-slate-100 w-2/4 before:block flex flex-col items-center"
+          className="before:content-user hover:cursor-pointer hover:bg-slate-100 w-32 flex flex-col text-center"
           onClick={() => setRole("user")}
         >
           User
         </p>
         <p
-          className="before:content-developer hover:cursor-pointer hover:bg-slate-100 w-2/4 before:block flex flex-col items-center"
+          className="before:content-developer hover:cursor-pointer hover:bg-slate-100 w-32 before:block flex flex-col text-center"
           onClick={() => setRole("developer")}
         >
           Developer
         </p>
         <p
-          className="before:content-admin hover:cursor-pointer hover:bg-slate-100 w-2/4 before:block flex flex-col items-center"
+          className="before:content-admin hover:cursor-pointer hover:bg-slate-100 before:block w-32 flex flex-col text-center"
           onClick={() => setRole("admin")}
         >
           Admin
